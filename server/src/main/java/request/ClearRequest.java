d0b9ef8731c7a4df336143d6da763b5b5b495da2
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-20 16:37:04 -0600

    Removed ClearRequest and ClearResult

--- server/src/main/java/request/ClearRequest.java
+++
@@ -1,7 +0,0 @@
-package request;
-
-/**
- * Request to clear the database. Removes all users, games, and authTokens
- */
-public record ClearRequest() {
-}
--- server/src/main/java/result/ClearResult.java
+++
@@ -1,7 +0,0 @@
-package result;
-
-/**
- * Result class for the clear endpoint.
- */
-public record ClearResult() {
-}
