400f08d130befcac13ba4bec6924cb43bc0506d5
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-20 16:02:47 -0600

    Made all UserDAOInterface methods potentially throw DataAccessExceptions

--- server/src/main/java/dataaccess/UserDAOInterface.java
+++ server/src/main/java/dataaccess/UserDAOInterface.java
@@ -6,19 +6,24 @@ public interface UserDAOInterface {
 
     /**
      * Clear all data from the database.
+     * @throws DataAccessException Indicates an error reaching the database.
      */
-    void clear();
+    void clear() throws DataAccessException;
 
     /**
      * Create a new user.
      * @param userData The data to add to the database.
+     * @throws DuplicateEntryException Indicates the username is already found in the database.
+     * @throws DataAccessException Indicates an error reaching the database.
      */
-    void createUser(UserData userData);
+    void createUser(UserData userData) throws DataAccessException;
 
     /**
      * Retrieve a user with the given username.
      * @param username The username to retrieve.
      * @return Userdata for the requested user, or null if not found.
+     * @throws EntryNotFoundException Indicates that the given username is not found in the database.
+     * @throws DataAccessException Indicates an error reaching the database.
      */
-    UserData getUser(String username);
+    UserData getUser(String username) throws DataAccessException;
 }
