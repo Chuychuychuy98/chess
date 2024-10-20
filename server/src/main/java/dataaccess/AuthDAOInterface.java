6bb87229fd28e099a2506d21464193fa6c07f78c
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-20 16:00:18 -0600

    Made all AuthDAOInterface methods potentially throw DataAccessExceptions

--- server/src/main/java/dataaccess/AuthDAOInterface.java
+++ server/src/main/java/dataaccess/AuthDAOInterface.java
@@ -6,26 +6,32 @@ public interface AuthDAOInterface {
 
     /**
      * Clear all data from the database.
+     * @throws DataAccessException Indicates an error reaching the database.
      */
-    void clear();
+    void clear() throws DataAccessException;
 
     /**
      * Create a new authorization in the database.
      * @param authData The authData to add to the database.
+     * @throws DataAccessException Indicates an error reaching the database.
      */
-    void createAuth(AuthData authData);
+    void createAuth(AuthData authData) throws DataAccessException;
 
     /**
      * Retrieve an authorization given an authToken.
      * @param authToken The token to search for.
-     * @return AuthData for the requested authToken, or null if not found.
+     * @return AuthData for the requested authToken.
+     * @throws EntryNotFoundException Indicates that the given authToken is not found in the database.
+     * @throws DataAccessException Indicates an error reaching the database.
      */
-    AuthData getAuth(String authToken);
+    AuthData getAuth(String authToken) throws DataAccessException;
 
     /**
      * Delete an authorization so that it is no longer valid.
      * @param authData The authorization to delete.
+     * @throws EntryNotFoundException Indicates that the given authData is not found in the database.
+     * @throws DataAccessException Indicates an error reaching the database.
      */
-    void deleteAuth(AuthData authData);
+    void deleteAuth(AuthData authData) throws DataAccessException;
 
 }
