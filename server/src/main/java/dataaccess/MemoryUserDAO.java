cdb7064e2d5e1bc993509b43d747efc82a90bf5e
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-21 18:29:01 -0600

    Implemented methods from UserDAO

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -2,22 +2,33 @@ package dataaccess;
 
 import model.UserData;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Memory implementation of UserDAO
  */
 public class MemoryUserDAO implements UserDAO{
-    @Override
-    public void clear() throws DataAccessException {
 
+    Map<String, UserData> database = new HashMap<>();
+
+    @Override
+    public void clear() {
+        database.clear();
     }
 
     @Override
-    public void createUser(UserData userData) throws DataAccessException, DuplicateEntryException {
-
+    public void createUser(UserData userData) throws DuplicateEntryException {
+        if (database.get(userData.username()) == null) {
+            throw new DuplicateEntryException(String.format("Username %s is taken.", userData.username()));
+        }
+        database.put(userData.username(), userData);
     }
 
     @Override
-    public UserData getUser(String username) throws DataAccessException, EntryNotFoundException {
-        return null;
+    public UserData getUser(String username) throws EntryNotFoundException {
+        UserData data = database.get(username);
+        if (data == null) throw new EntryNotFoundException(String.format("No user found with username %s.", username));
+        return data;
     }
 }
