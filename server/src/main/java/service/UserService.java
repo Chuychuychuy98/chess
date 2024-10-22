072495ab18935d91894baae002d9871a044b7b2a
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-21 19:40:05 -0600

    Added javadoc comments to register() and login()

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -26,6 +26,14 @@ public class UserService {
         this.authDAO = authDAO;
     }
 
+    /**
+     * Register a new user.
+     * @param request The request containing the username, password, and email to register.
+     * @return AuthTokenResult containing the new authToken and the username associated with it.
+     * @throws DuplicateEntryException Indicates that the username was already taken or that the random authToken was not unique.
+     * @throws DataAccessException Indicates an error reaching the database.
+     * @throws EntryNotFoundException Indicates that the user was added, but adding the authToken failed, then the user could not be found when trying to remove it.
+     */
     public AuthTokenResult register(RegisterRequest request) throws DuplicateEntryException, DataAccessException, EntryNotFoundException {
         UserData newUser = new UserData(request.username(), request.password(), request.email());
         AuthData newAuth = new AuthData(UUID.randomUUID().toString(), newUser.username());
@@ -40,6 +48,14 @@ public class UserService {
         return new AuthTokenResult(newAuth);
     }
 
+    /**
+     * Log a user in and associate it with a new authToken.
+     * @param request The request containing the username and password to log in.
+     * @return AuthTokenResult containing the new authToken and the username associated with it.
+     * @throws DataAccessException Indicates an error reaching the database.
+     * @throws UnauthorizedException Indicates that the username does not exist, or the username and password do not match.
+     * @throws DuplicateEntryException Indicates that the generated authToken was not unique.
+     */
     public AuthTokenResult login(LoginRequest request) throws DataAccessException, UnauthorizedException, DuplicateEntryException {
         UserData user;
         try {
