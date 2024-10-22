80eb6c155b031a6753ecaab5b1e7de441b8c7248
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-21 18:25:57 -0600

    Implemented methods from GameDAO

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -3,32 +3,42 @@ package dataaccess;
 import chess.ChessGame;
 import model.GameData;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Memory implementation of GameDAO
  */
 public class MemoryGameDAO implements GameDAO{
-    @Override
-    public void clear() throws DataAccessException {
 
+    Map<Integer, GameData> database = new HashMap<>();
+    @Override
+    public void clear() {
+        database.clear();
     }
 
     @Override
-    public void createGame(GameData gameData) throws DataAccessException, DuplicateEntryException {
-
+    public void createGame(GameData gameData) throws DuplicateEntryException {
+        if (database.get(gameData.gameID()) != null) throw new DuplicateEntryException(String.format("Game with id %d already exists.", gameData.gameID()));
+        database.put(gameData.gameID(), gameData);
     }
 
     @Override
-    public GameData getGame(int gameID) throws DataAccessException, EntryNotFoundException {
-        return null;
+    public GameData getGame(int gameID) throws EntryNotFoundException {
+        GameData data = database.get(gameID);
+        if (data == null) throw new EntryNotFoundException(String.format("Game with ID %d not found.", gameID));
+        return data;
     }
 
     @Override
-    public GameData[] listGames() throws DataAccessException {
-        return new GameData[0];
+    public GameData[] listGames() {
+        return database.values().toArray(new GameData[0]);
     }
 
     @Override
-    public void updateGame(int gameID, ChessGame game) throws DataAccessException, EntryNotFoundException {
-
+    public void updateGame(int gameID, ChessGame game) throws EntryNotFoundException {
+        GameData data = database.get(gameID);
+        if (data == null) throw new EntryNotFoundException(String.format("Game with id %d not found.", gameID));
+        data.game().updateGame(game);
     }
 }
