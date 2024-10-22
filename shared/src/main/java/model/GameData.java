26f8192486f6324126b9c0d19324afbb0c115f58
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-21 21:45:28 -0600

    Added newPlayer()

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -10,4 +10,19 @@ public record GameData(int gameID, String whiteUsername, String blackUsername, S
     public GameData(String whiteUsername, String blackUsername, String gameName, ChessGame game) {
         this(nextID++, whiteUsername, blackUsername, gameName, game);
     }
+
+    /**
+     * Return a copy of this GameData with the player of the given color replaced with the new username.
+     * @param color The color to replace.
+     * @param newUsername The username to replace the old one.
+     * @return The new GameData.
+     */
+    public GameData newPlayer(ChessGame.TeamColor color, String newUsername) {
+        if (color == ChessGame.TeamColor.BLACK) {
+            return new GameData(gameID, whiteUsername, newUsername, gameName, game);
+        }
+        else {
+            return new GameData(gameID, newUsername, blackUsername, gameName, game);
+        }
+    }
 }
