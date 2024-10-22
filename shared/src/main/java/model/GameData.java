6bd3a2a5c7b12688dc09f7fa661da0241d3a3f38
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-22 16:59:58 -0600

    Correct GameData ids to start at 1 to match requirement

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -5,7 +5,7 @@ import chess.ChessGame;
 
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
-    private static int nextID = 0;
+    private static int nextID = 1;
 
     public GameData(String whiteUsername, String blackUsername, String gameName, ChessGame game) {
         this(nextID++, whiteUsername, blackUsername, gameName, game);
