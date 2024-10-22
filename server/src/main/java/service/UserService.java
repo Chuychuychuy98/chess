de68b6d93af3f12d36b268ba02a67cb163e575ac
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-21 18:59:50 -0600

    Added register method

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,7 +1,12 @@
 package service;
 
-import dataaccess.AuthDAO;
-import dataaccess.UserDAO;
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import request.RegisterRequest;
+import result.AuthTokenResult;
+
+import java.util.UUID;
 
 /**
  * Service class for register, login, and logout endpoints.
@@ -18,5 +23,19 @@ public class UserService {
     public UserService(UserDAO userDAO, AuthDAO authDAO) {
         this.userDAO = userDAO;
         this.authDAO = authDAO;
+    }
+
+    public AuthTokenResult register(RegisterRequest request) throws DuplicateEntryException, DataAccessException, EntryNotFoundException {
+        UserData newUser = new UserData(request.username(), request.password(), request.email());
+        AuthData newAuth = new AuthData(UUID.randomUUID().toString(), newUser.username());
+        userDAO.createUser(newUser);
+        try {
+            authDAO.createAuth(newAuth);
+        }
+        catch (Exception e) {
+            userDAO.removeUser(newUser.username());
+            throw e;
+        }
+        return new AuthTokenResult(newAuth);
     }
 }
