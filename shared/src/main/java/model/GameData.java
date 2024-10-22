90bd6783b9ea0b3c464446ffc04b7eb3653bddb9
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-21 20:59:05 -0600

    Added equalsNoID() method for use in testing

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -10,4 +10,11 @@ public record GameData(int gameID, String whiteUsername, String blackUsername, S
     public GameData(String whiteUsername, String blackUsername, String gameName, ChessGame game) {
         this(nextID++, whiteUsername, blackUsername, gameName, game);
     }
+
+    public boolean equalsNoID(GameData other) {
+            return other.whiteUsername.equals(this.whiteUsername) &&
+                    other.blackUsername.equals(this.blackUsername) &&
+                    other.gameName.equals(this.gameName) &&
+                    other.game.equals(this.game);
+    }
 }
