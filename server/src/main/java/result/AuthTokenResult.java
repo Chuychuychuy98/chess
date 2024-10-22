1111903ced01fcae58437965a89901098cb716c3
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-21 18:51:01 -0600

    Added constructor for AuthTokenResult that takes in AuthData

--- server/src/main/java/result/AuthTokenResult.java
+++ server/src/main/java/result/AuthTokenResult.java
@@ -1,9 +1,19 @@
 package result;
 
+import model.AuthData;
+
 /**
  * Result class for endpoints that return a username and authToken.
  * @param username The username being returned.
  * @param authToken The authToken being returned.
  */
 public record AuthTokenResult(String username, String authToken) {
+
+    /**
+     * Construct an AuthTokenResult from the given AuthData.
+     * @param data The AuthData to construct the AuthTokenResult from.
+     */
+    public AuthTokenResult(AuthData data) {
+        this(data.username(), data.authToken());
+    }
 }
