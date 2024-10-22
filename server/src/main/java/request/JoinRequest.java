ab69c01473a3c590b9456219606d43a58ba2c985
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-21 21:24:54 -0600

    Corrected JoinRequest to take a game's ID rather than its name

--- server/src/main/java/request/JoinRequest.java
+++ server/src/main/java/request/JoinRequest.java
@@ -6,7 +6,7 @@ import chess.ChessGame;
  * Request class for join endpoint.
  * @param authToken The authToken to validate.
  * @param color The requested team color.
- * @param gameName The name of the game to be joined.
+ * @param gameID The ID of the game to be joined.
  */
-public record JoinRequest(String authToken, ChessGame.TeamColor color, String gameName) {
+public record JoinRequest(String authToken, ChessGame.TeamColor color, int gameID) {
 }
