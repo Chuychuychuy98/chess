85950fc80fe87401b6419fb45db62809f8c89483
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-21 20:31:36 -0600

    Added GameData() constructor that automatically generates a new ID

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -2,5 +2,12 @@ package model;
 
 import chess.ChessGame;
 
+
+
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    private static int nextID = 0;
+
+    public GameData(String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+        this(nextID++, whiteUsername, blackUsername, gameName, game);
+    }
 }
