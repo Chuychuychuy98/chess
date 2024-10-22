620872224b31fdb48e91611e46840c60b71bc9cf
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-21 21:22:38 -0600

    Added TeamColorTakenException to indicate that the color is already taken

---
+++ server/src/main/java/service/TeamColorTakenException.java
@@ -0,0 +1,10 @@
+package service;
+
+/**
+ * Indicates that the request to join a game could not be fulfilled because the requested team color was already taken.
+ */
+public class TeamColorTakenException extends Exception{
+    public TeamColorTakenException(String message) {
+        super(message);
+    }
+}
