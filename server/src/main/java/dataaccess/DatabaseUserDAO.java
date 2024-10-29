419779e6993cc6575a7eaef0315c1ffbb3463413
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-29 15:48:52 -0600

    Implemented createUser()

--- server/src/main/java/dataaccess/DatabaseUserDAO.java
+++ server/src/main/java/dataaccess/DatabaseUserDAO.java
@@ -3,6 +3,12 @@ package dataaccess;
 import exceptions.DuplicateEntryException;
 import exceptions.EntryNotFoundException;
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DatabaseUserDAO implements UserDAO {
     @Override
@@ -12,7 +18,26 @@ public class DatabaseUserDAO implements UserDAO {
 
     @Override
     public void createUser(UserData userData) throws DataAccessException, DuplicateEntryException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT username FROM user WHERE username=?")) {
+                ps.setString(1, userData.username());
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        throw new DuplicateEntryException("Error: already taken");
+                    }
+                }
+            }
+            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO user (username, password, email) VALUES (?, ?, ?)")) {
+                ps.setString(1, userData.username());
+                ps.setString(2, userData.password());
+                ps.setString(3, userData.email());
 
+                ps.executeUpdate();
+            }
+        }
+        catch (SQLException ex) {
+            throw new DataAccessException(String.format("Error: %s", ex.getMessage()));
+        }
     }
 
     @Override
