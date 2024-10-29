cadd5d0e19f5f45c568ae9372cd34b5cfdcc7e2c
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-28 20:22:17 -0600

    Created tests for clearing and adding to authToken database

---
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -0,0 +1,72 @@
+package dataaccess;
+
+import exceptions.DuplicateEntryException;
+import model.AuthData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DataAccessTests {
+
+    private static AuthDAO authDAO;
+    private static GameDAO gameDAO;
+    private static UserDAO userDAO;
+
+    @BeforeAll
+    public static void init() {
+        authDAO = new DatabaseAuthDAO();
+        gameDAO = new DatabaseGameDAO();
+        userDAO = new DatabaseUserDAO();
+    }
+
+    @BeforeEach
+    public void clearDatabase() throws DataAccessException, SQLException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement("TRUNCATE TABLE auth")) {
+                ps.executeUpdate();
+            }
+            try (PreparedStatement ps = conn.prepareStatement("TRUNCATE TABLE game")) {
+                ps.executeUpdate();
+            }
+            try (PreparedStatement ps = conn.prepareStatement("TRUNCATE TABLE user")) {
+                ps.executeUpdate();
+            }
+        }
+    }
+
+    @Test
+    public void addAuthSuccess() throws DataAccessException, SQLException {
+        Assertions.assertDoesNotThrow(() -> authDAO.createAuth(new AuthData("abc", "user")));
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT authtoken FROM auth WHERE authtoken='abc'")) {
+                ResultSet rs = ps.executeQuery();
+                Assertions.assertTrue(rs.next());
+            }
+        }
+    }
+
+    @Test
+    public void addAuthDuplicate() {
+        Assertions.assertDoesNotThrow(() -> authDAO.createAuth(new AuthData("abc", "user")));
+        Assertions.assertThrows(DuplicateEntryException.class,
+                () -> authDAO.createAuth(new AuthData("abc", "user")));
+    }
+
+    @Test
+    public void clearAuth() throws DataAccessException, SQLException {
+        Assertions.assertDoesNotThrow(() -> authDAO.createAuth(new AuthData("abc", "user")));
+        Assertions.assertDoesNotThrow(() -> authDAO.clear());
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT authtoken FROM auth")) {
+                ResultSet rs = ps.executeQuery();
+                Assertions.assertFalse(rs.next());
+            }
+        }
+    }
+}
