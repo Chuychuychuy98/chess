91603d132788536d9074c59cb7109c6db290938a
Chuychuychuy98 <rowebot98@gmail.com>
2024-10-29 15:45:19 -0600

    Hash password before storing or using it anywhere

--- server/src/main/java/request/LoginRequest.java
+++ server/src/main/java/request/LoginRequest.java
@@ -1,9 +1,15 @@
 package request;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 /**
  * Request class for login endpoint.
  * @param username The username trying to login.
- * @param password The user's password.
+ * @param password The user's password, which is hashed before being stored.
  */
 public record LoginRequest(String username, String password) {
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 }
