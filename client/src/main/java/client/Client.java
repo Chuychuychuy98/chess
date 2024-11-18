5ecda91978530bad7d806b0980b38050010478f4
Chuychuychuy98 <rowebot98@gmail.com>
2024-11-17 19:03:21 -0700

    Implemented Register option

---
+++ client/src/main/java/client/Client.java
@@ -0,0 +1,126 @@
+package client;
+
+import exceptions.ResponseException;
+import serverfacade.ServerFacade;
+import ui.EscapeSequences;
+
+import java.util.Scanner;
+
+public class Client {
+    String authToken = null;
+    ServerFacade server;
+
+    public Client(ServerFacade server) {
+        this.server = server;
+    }
+
+    public void start() {
+        System.out.println("♕ Welcome to 240 Chess. Type Help to get started. ♕");
+        beforeLogin();
+    }
+
+    public void beforeLogin() {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.print("[LOGGED_OUT] >>> ");
+            String userInput = in.next();
+            switch (userInput) {
+                case "help":
+                    helpBeforeLogin();
+                    break;
+                case "login":
+                    if (login(in)) {
+                        afterLogin();
+                    }
+                    else {
+                        System.out.println("Failed to log in!");
+                    }
+                    break;
+                case "register":
+                    if (register(in)) {
+                        afterLogin();
+                    }
+                    else {
+                        System.out.println("Failed to register!");
+                    }
+                    break;
+                case "quit":
+                    return;
+                default:
+                    System.out.println("Unrecognized command. For a list of available commands, type \"help\"");
+            }
+        }
+    }
+
+    public void afterLogin() {
+
+    }
+
+    public void helpBeforeLogin() {
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "login <USERNAME> <PASSWORD>"
+                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - create an account");
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "register <USERNAME> <PASSWORD> <EMAIL>"
+                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - log into your account to play chess");
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "quit"
+                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - exit the chess client");
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "help"
+                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - print this help message");
+        System.out.print(EscapeSequences.RESET_TEXT_COLOR);
+    }
+
+    public void helpAfterLogin() {
+
+    }
+
+    public boolean login(Scanner in) {
+        return false;
+    }
+
+    public boolean register(Scanner in) {
+        if (in.hasNext()) {
+            String username = in.next();
+            if (in.hasNext()) {
+                String password = in.next();
+                if (in.hasNext()) {
+                    String email = in.next();
+                    try {
+                        authToken = server.register(username, password, email).authToken();
+                        return true;
+                    }
+                    catch (ResponseException e) {
+                        System.out.printf("Error: %s%n", e.getMessage());
+                        return false;
+                    }
+                }
+                else {
+                    System.out.println("Error: missing email address.");
+                    return false;
+                }
+            }
+            else {
+                System.out.println("Error: missing password and email address.");
+                return false;
+            }
+        }
+        else {
+            return registerGetInfo(in);
+        }
+    }
+
+    public boolean registerGetInfo(Scanner in) {
+        System.out.print("Username: ");
+        String username = in.next();
+        System.out.print("Password: ");
+        String password = in.next();
+        System.out.print("Email: ");
+        String email = in.next();
+        try {
+            authToken = server.register(username, password, email).authToken();
+            return true;
+        }
+        catch (ResponseException e) {
+            System.out.printf("Error: %s%n", e.getMessage());
+            return false;
+        }
+    }
+}
