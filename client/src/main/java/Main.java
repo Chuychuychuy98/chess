d3cda192fd2237922c39d671c7169b04942308e1
Chuychuychuy98 <rowebot98@gmail.com>
2024-11-17 18:07:30 -0700

    Implemented quit and help

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,26 +1,32 @@
+import ui.EscapeSequences;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("♕ Welcome to 240 Chess. Type Help to get started. ♕");
-
         beforeLogin();
     }
 
     public static void beforeLogin() {
         Scanner in = new Scanner(System.in);
         while (true) {
-            System.out.println("[LOGGED_OUT] >>> ");
-            String userInput = in.next("[ \n]");
-            switch (userInput) {
+            System.out.print("[LOGGED_OUT] >>> ");
+            String userInput = in.nextLine();
+            String command = userInput;
+            int commandEnd = userInput.indexOf(" ");
+            if (commandEnd != -1) {
+                command = userInput.substring(0, commandEnd);
+            }
+            switch (command) {
                 case "help":
                     helpBeforeLogin();
                     break;
                 case "login":
-                    login(in);
+                    login(userInput);
                     break;
                 case "register":
-                    register(in);
+                    register(userInput);
                     break;
                 case "quit":
                     return;
@@ -31,18 +37,26 @@ public class Main {
     }
 
     public static void helpBeforeLogin() {
-        System.out.println();
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "login <USERNAME> <PASSWORD>"
+                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - create an account");
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "register <USERNAME> <PASSWORD> <EMAIL>"
+                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - log into your account to play chess");
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "quit"
+                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - exit the chess client");
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "help"
+                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - print this help message");
+        System.out.print(EscapeSequences.RESET_TEXT_COLOR);
     }
 
     public static void helpAfterLogin() {
 
     }
 
-    public static void login(Scanner in) {
+    public static void login(String input) {
 
     }
 
-    public static void register(Scanner in) {
+    public static void register(String input) {
 
     }
 }
