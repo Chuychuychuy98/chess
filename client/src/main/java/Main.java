47c81cc8838a45c5639b737b4c0518728ffe3a6c
Chuychuychuy98 <rowebot98@gmail.com>
2024-11-17 17:52:38 -0700

    Added pre-login method stubs

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("♕ Welcome to 240 Chess. Type Help to get started. ♕");
+
+        beforeLogin();
+    }
+
+    public static void beforeLogin() {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.println("[LOGGED_OUT] >>> ");
+            String userInput = in.next("[ \n]");
+            switch (userInput) {
+                case "help":
+                    helpBeforeLogin();
+                    break;
+                case "login":
+                    login(in);
+                    break;
+                case "register":
+                    register(in);
+                    break;
+                case "quit":
+                    return;
+                default:
+                    System.out.println("Unrecognized command. For a list of available commands, type \"help\"");
+            }
+        }
+    }
+
+    public static void helpBeforeLogin() {
+        System.out.println();
+    }
+
+    public static void helpAfterLogin() {
+
+    }
+
+    public static void login(Scanner in) {
+
+    }
+
+    public static void register(Scanner in) {
+
     }
 }
