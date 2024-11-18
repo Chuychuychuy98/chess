d702f934da18a8fb2541b449a95cef862eca347a
Chuychuychuy98 <rowebot98@gmail.com>
2024-11-17 18:40:44 -0700

    Created ResponseException class

---
+++ shared/src/main/java/exceptions/ResponseException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class ResponseException extends Exception {
+    int status;
+
+    public ResponseException(int status, String message) {
+        super(message);
+        this.status = status;
+    }
+}
