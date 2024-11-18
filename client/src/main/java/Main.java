c8b46fe528fb1161ca2783256ea0bae81e5d969a
Chuychuychuy98 <rowebot98@gmail.com>
2024-11-17 19:11:26 -0700

    Fixed errors in Main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,62 +1,28 @@
+import client.Client;
+import serverfacade.ServerFacade;
 import ui.EscapeSequences;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("♕ Welcome to 240 Chess. Type Help to get started. ♕");
-        beforeLogin();
-    }
-
-    public static void beforeLogin() {
-        Scanner in = new Scanner(System.in);
-        while (true) {
-            System.out.print("[LOGGED_OUT] >>> ");
-            String userInput = in.nextLine();
-            String command = userInput;
-            int commandEnd = userInput.indexOf(" ");
-            if (commandEnd != -1) {
-                command = userInput.substring(0, commandEnd);
-            }
-            switch (command) {
-                case "help":
-                    helpBeforeLogin();
-                    break;
-                case "login":
-                    login(userInput);
-                    break;
-                case "register":
-                    register(userInput);
-                    break;
-                case "quit":
-                    return;
-                default:
-                    System.out.println("Unrecognized command. For a list of available commands, type \"help\"");
-            }
+        String url;
+        if (args.length == 1) {
+            url = "localhost";
         }
+        else if (args.length == 2) {
+            url = args[1];
+        }
+        else if (args.length == 3) {
+            url = args[1] + ":" + args[2];
+        }
+        else {
+            System.out.println("Usage: chess [server name] [port]");
+            return;
+        }
+        Client client = new Client(new ServerFacade(url));
+        client.start();
     }
 
-    public static void helpBeforeLogin() {
-        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "login <USERNAME> <PASSWORD>"
-                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - create an account");
-        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "register <USERNAME> <PASSWORD> <EMAIL>"
-                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - log into your account to play chess");
-        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "quit"
-                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - exit the chess client");
-        System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "help"
-                + EscapeSequences.SET_TEXT_COLOR_MAGENTA + " - print this help message");
-        System.out.print(EscapeSequences.RESET_TEXT_COLOR);
-    }
 
-    public static void helpAfterLogin() {
-
-    }
-
-    public static void login(String input) {
-
-    }
-
-    public static void register(String input) {
-
-    }
 }
