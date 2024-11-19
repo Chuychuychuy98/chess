9d72e649b4e1a8947d74c1d61fc6947c02237520
Chuychuychuy98 <rowebot98@gmail.com>
2024-11-18 21:42:51 -0700

    Fixed array error in main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -7,14 +7,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String url;
-        if (args.length == 1) {
+        if (args.length == 0) {
             url = "localhost";
         }
-        else if (args.length == 2) {
-            url = args[1];
+        else if (args.length == 1) {
+            url = args[0];
         }
-        else if (args.length == 3) {
-            url = args[1] + ":" + args[2];
+        else if (args.length == 2) {
+            url = args[0] + ":" + args[1];
         }
         else {
             System.out.println("Usage: chess [server name] [port]");
